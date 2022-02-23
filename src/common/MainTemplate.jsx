48ab30a9c10a99c3860bf5c1b9a46becf5dcdb19
igor santos <igorsantos381@gmail.com>
2022-02-22 23:46:46 -0300

    navbar added

--- src/common/MainTemplate.jsx
+++ src/common/MainTemplate.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
+const HomeLink = styled(Link)`
+    color: yellow;
+    padding: 10px 30px;
+    text-decoration: none;
+    border-radius: 10px;
+    transition: background-color 0.5s;
+    &:hover{
+        background-color: rgba(102, 95, 95, 0.356);
+    }
+`
+const NavBar = styled.div`
+    display: flex;
+    justify-content: center;
+`
 const MainTemplate = (props) => (
     <div style={{ color: 'yellow' }}>
         <header><h1>{props.title}</h1></header>
-        <div style={{border:'1px solid red'}}>teste</div>
+        <NavBar>
+            <HomeLink to='/'>
+                <h3>Home</h3>
+            </HomeLink>
+        </NavBar>
         {props.children}
     </div>
 )
