ccc47c03e29d806c7badae4d816523d4bad1bef4
igor-santos <igor.santos@hp.com>
2022-02-24 17:33:46 -0300

    adding collapse components

--- src/model/Pages/Movie/Movie.jsx
+++ src/model/Pages/Movie/Movie.jsx
@@ -8,11 +8,21 @@ import CharactersCard from '../../components/CharactersCard'
 import PlanetsCard from '../../components/PlanetsCard'
 import SpeciesCard from '../../components/SpeciesCard'
 import MainTemplate from '../../../common/MainTemplate'
+import styled from 'styled-components'
+import { css } from 'styled-components'
+
+const Collapse = styled.div`
+    ${ props => css`
+        height: ${props.height};
+        overflow: hidden;
+        transition: height 1s;
+    `};
+`
 
 const Movie = () => {
     const { id } = useParams()
     const [data, setData] = useState(null)
-    const episodes = ['I', 'II', 'III', 'IV', 'V', 'VI']
+    const [showCharacters, setShowCharacters] = useState(false);
 
     useEffect(
         () => {
@@ -24,15 +34,19 @@ const Movie = () => {
         },
         [setData]
     )
+
+    const handleCharactersClick = () => setShowCharacters(curr => !curr)
+
     return (
         <SkeletonTheme baseColor='rgba(102, 95, 95, 0.356)' highlightColor='#FFF'>
             <MainTemplate title={data && data.title}>
                 <div className='flexMovie'>
                     <div className='.flexItemMovie'>
                         {/* <Card title="Characters" url={}></Card> */}
-                        <Card title="Characters"></Card>
-
-                        <CharactersCard urls={data && data.characters}></CharactersCard>
+                        <Card collapsible onClick={handleCharactersClick} title="Characters" showCharacters={showCharacters}></Card>
+                        <Collapse height={showCharacters ? '0' : '100%'}>
+                            <CharactersCard urls={data && data.characters}></CharactersCard>
+                        </Collapse>
                     </div>
                     
                     <div className='.flexItemMovie'>
--- src/model/components/Card.jsx
+++ src/model/components/Card.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 
-const Card = (props) => {
+const Card = ({collapsible, onClick,...props}) => {
     const [data, setData] = useState(null)
 
     useEffect(() => {
@@ -13,9 +13,17 @@ const Card = (props) => {
             })
     }, [setData])
 
+    const handleClick = () => {
+        onClick()
+    }
+
     return (
         <div className='card'>
+            <div style={{ display:'flex', justifyContent: 'center'}}>
             {props.url ? <h3 style={{color:"cyan"}}>{data && data.name}</h3> : <h3>{props.title}</h3>}
+            
+                {collapsible && <button style={{marginLeft: '20px', border: 'none', borderRadius: '10px', background: 'none', fontSize: '1em', color: 'yellow'}} onClick={handleClick}>{props.showCharacters ? <span>&#8592;</span> : <span>&#8595;</span>}</button>}
+            </div>
             
         </div>
     )
