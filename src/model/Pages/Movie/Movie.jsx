1f41eb2a57e4ff703a550137473764c6ffc391c7
igor-santos <igor.santos@hp.com>
2022-02-25 14:29:51 -0300

    continue work on collapse

--- src/model/Pages/Movie/Movie.jsx
+++ src/model/Pages/Movie/Movie.jsx
@@ -15,7 +15,6 @@ const Collapse = styled.div`
     ${ props => css`
         height: ${props.height};
         overflow: hidden;
-        transition: height 1s;
     `};
 `
 
@@ -23,6 +22,8 @@ const Movie = () => {
     const { id } = useParams()
     const [data, setData] = useState(null)
     const [showCharacters, setShowCharacters] = useState(false);
+    const [showPlanets, setShowPlanets] = useState(false);
+    const [showSpecies, setShowSpecies] = useState(false);
 
     useEffect(
         () => {
@@ -32,31 +33,37 @@ const Movie = () => {
                     setData(res)
                 })
         },
-        [setData]
+        [id]
     )
 
     const handleCharactersClick = () => setShowCharacters(curr => !curr)
-
+    const handlePlanetsClick = () => setShowPlanets(curr => !curr)
+    const handleSpeciesClick = () => setShowSpecies(curr => !curr)
     return (
         <SkeletonTheme baseColor='rgba(102, 95, 95, 0.356)' highlightColor='#FFF'>
             <MainTemplate title={data && data.title}>
                 <div className='flexMovie'>
                     <div className='.flexItemMovie'>
                         {/* <Card title="Characters" url={}></Card> */}
-                        <Card collapsible onClick={handleCharactersClick} title="Characters" showCharacters={showCharacters}></Card>
-                        <Collapse height={showCharacters ? '0' : '100%'}>
+                        <Card collapsible onClick={handleCharactersClick} title="Characters" show={showCharacters}></Card>
+                        <Collapse height={showCharacters ? '100%' : '0'}>
                             <CharactersCard urls={data && data.characters}></CharactersCard>
                         </Collapse>
                     </div>
                     
                     <div className='.flexItemMovie'>
-                        <Card title="Planets"></Card>
-                        {data && data.planets.map(planet => <PlanetsCard key={planet} url={planet}></PlanetsCard>)}
+                        <Card collapsible onClick={handlePlanetsClick} title="Planets" show={showPlanets}></Card>
+                        <Collapse height={showPlanets ? '100%' : '0'}>
+                            {data && data.planets.map(planet => <PlanetsCard key={planet} url={planet}></PlanetsCard>)}
+                        </Collapse>
+                        
                     </div>
 
                     <div className='.flexItemMovie'>
-                        <Card title="Species"></Card>
-                        {data && data.species.map(specie => <SpeciesCard key={specie} url={specie}></SpeciesCard>)}
+                        <Card collapsible onClick={handleSpeciesClick} show={showSpecies} title="Species"></Card>
+                        <Collapse height={showSpecies ? '100%' : '0'}>
+                            {data && data.species.map(specie => <SpeciesCard key={specie} url={specie}></SpeciesCard>)}
+                        </Collapse>
                     </div>
                 </div>
             </MainTemplate>
--- src/model/components/Card.jsx
+++ src/model/components/Card.jsx
@@ -1,4 +1,14 @@
 import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+
+const ArrowButton = styled.button`
+    margin-left: 20px;
+    border: none;
+    border-radius: 10px;
+    background: none;
+    font-size: 1em;
+    color: yellow;
+`
 
 const Card = ({collapsible, onClick,...props}) => {
     const [data, setData] = useState(null)
@@ -11,7 +21,7 @@ const Card = ({collapsible, onClick,...props}) => {
                 setData(res)
 
             })
-    }, [setData])
+    }, [props.url])
 
     const handleClick = () => {
         onClick()
@@ -21,8 +31,7 @@ const Card = ({collapsible, onClick,...props}) => {
         <div className='card'>
             <div style={{ display:'flex', justifyContent: 'center'}}>
             {props.url ? <h3 style={{color:"cyan"}}>{data && data.name}</h3> : <h3>{props.title}</h3>}
-            
-                {collapsible && <button style={{marginLeft: '20px', border: 'none', borderRadius: '10px', background: 'none', fontSize: '1em', color: 'yellow'}} onClick={handleClick}>{props.showCharacters ? <span>&#8592;</span> : <span>&#8595;</span>}</button>}
+            {collapsible && <ArrowButton  onClick={handleClick}>{props.show ? <span>&#8595;</span> : <span>&#8592;</span>}</ArrowButton>}
             </div>
             
         </div>
